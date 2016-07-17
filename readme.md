8dde17451a38bdd41cc0721ce44bf38d6d4aca0a
zo17 <juliana.leon@global.tesco.org>
2016-07-17 14:43:21 +0100

    add server

---
+++ readme.md
@@ -0,0 +1,10 @@
+<a href="">DEMO APP </a>
+
+<h1>React button hover inline </h1>
+
+
+
+run npm install
+
+Run server use => node server.js
+Run webpack use => webpack -w
--- server.js
+++ server.js
@@ -2,10 +2,27 @@ var express = require('express');
 
 //Creat the APP
 
+// var app = express();
+// app.use(express.static('public'));
+//
+// app.listen(8000, function(){
+//   console.log('Express server is up on port 8000');
+// });
+
+
 var app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(function (req, res, next){
+  if (req.headers['x-forwarded-proto'] === 'https') {
+      res.redirect('http://' + req.hostname + req.url);
+  } else {
+    next();
+  }
+});
 
 app.use(express.static('public'));
 
-app.listen(8000, function(){
-  console.log('Express server is up on port 8000');
+app.listen(PORT, function () {
+  console.log('Express server is up on port ' + PORT);
 });
